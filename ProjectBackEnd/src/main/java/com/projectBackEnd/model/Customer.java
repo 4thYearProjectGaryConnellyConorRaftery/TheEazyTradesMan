7c93423de2876b91af0d4085bb81b85a32471618
ZqhKHP <G00336837@gmit.ie>
2018-11-02 15:44:38 +0000

    adding a model for the customer object

---
+++ ProjectBackEnd/src/main/java/com/projectBackEnd/model/Customer.java
@@ -0,0 +1,71 @@
+package com.projectBackEnd.model;
+
+import java.util.Arrays;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c")
+public class Customer {
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+	private String firstName;
+	private String secondName;
+	private String[] address;
+	private int age;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String[] getAddress() {
+		return address;
+	}
+
+	public void setAddress(String[] address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", secondName=" + secondName + ", address="
+				+ Arrays.toString(address) + ", age=" + age + "]";
+	}
+
+}
