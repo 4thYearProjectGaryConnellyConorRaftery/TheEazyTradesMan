c8804fe75e8fff584d7fecab83bc7c3f8b96a894
ZqhKHP <G00336837@gmit.ie>
2018-11-02 18:26:11 +0000

    worker model

--- ProjectBackEnd/src/main/java/com/projectBackEnd/model/Worker.java
+++ ProjectBackEnd/src/main/java/com/projectBackEnd/model/Worker.java
@@ -1,7 +1,5 @@
 package com.projectBackEnd.model;
 
-//import java.util.Arrays;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,9 +8,9 @@ import javax.persistence.NamedQuery;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
-@NamedQuery(name = "Customer.findAll", query = "SELECT c FROM Customer c")
-public class Customer {
-
+@NamedQuery(name = "Worker.findAll", query = "SELECT w FROM Worker w")
+public class Worker {
+	
 	@Id
 	@GeneratedValue(generator = "uuid")
 	@GenericGenerator(name = "uuid", strategy = "uuid2")
@@ -21,53 +19,56 @@ public class Customer {
 	private String secondName;
 	private String address;
 	private int age;
-
+	private String trade;
+	private double rating;
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public String getSecondName() {
 		return secondName;
 	}
-
 	public void setSecondName(String secondName) {
 		this.secondName = secondName;
 	}
-
-	
-
 	public String getAddress() {
 		return address;
 	}
-
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
+	public String getTrade() {
+		return trade;
+	}
+	public void setTrade(String trade) {
+		this.trade = trade;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", firstName=" + firstName + ", secondName=" + secondName + ", address="
-				+ ", age=" + age + "]";
+		return "Worker [id=" + id + ", firstName=" + firstName + ", secondName=" + secondName + ", address=" + address
+				+ ", age=" + age + ", trade=" + trade + ", rating=" + rating + "]";
 	}
+	
+	
 
 }
