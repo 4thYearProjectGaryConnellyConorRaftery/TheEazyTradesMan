6bee9e49a0b5790ceb6ca8eeb9ddcd71947449db
ZqhKHP <G00336837@gmit.ie>
2019-02-09 15:18:32 +0000

    Adding a form for posting a job.

--- ProjectFrontEnd/src/app/Customer/post-job.component.ts
+++ ProjectFrontEnd/src/app/Customer/post-job.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-job',
@@ -10,6 +11,10 @@ export class PostJobComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  postJob(postJobForm: NgForm): void{
+    console.log(postJobForm.value);
   }
 
 }
