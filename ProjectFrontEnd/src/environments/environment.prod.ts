74af48938da5dd1e1453c1b0817e4a0ace83e238
conor <c-raftery@hotmail.com>
2019-02-11 23:52:13 +0000

    Hosting configured, need to route correctly

--- ProjectFrontEnd/src/environments/environment.prod.ts
+++ ProjectFrontEnd/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCKMcXk3ixcUH6CiEglf8cWBHwetaUlQVs",
+    authDomain: "theeazytradesman.firebaseapp.com",
+    databaseURL: "https://theeazytradesman.firebaseio.com",
+    projectId: "theeazytradesman",
+    storageBucket: "theeazytradesman.appspot.com",
+    messagingSenderId: "838547571245"
+  }
 };
