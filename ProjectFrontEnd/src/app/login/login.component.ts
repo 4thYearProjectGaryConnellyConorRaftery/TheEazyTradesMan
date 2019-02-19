c30f77e0fa4629ab7c995930e45270730cb58d94
ZqhKHP <G00336837@gmit.ie>
2019-02-19 00:07:31 +0000

    Merging ConnectDatabases branch.

--- ProjectFrontEnd/src/app/login/login.component.ts
+++ ProjectFrontEnd/src/app/login/login.component.ts
@@ -70,14 +70,7 @@ export class LoginComponent {
       this.getUser(firebase.auth().currentUser.uid.toString());
 
       
-      /*Get UID from MongoDB, and switch landing page respectively
-      if(UID is worker){
-        this.router.navigate(['/listWorkerJobs']);
-      }else{
-        this.router.navigate(['/listWorkerCustomerJobs']);
-      }
-      */
-     //Remove line below when routing is done correctly
+     
       this.router.navigate(['/user']);
       
     }, err => {
