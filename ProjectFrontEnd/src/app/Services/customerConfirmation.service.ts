ed97a83c7547615efdbb3ba9297d41d4c78be689
ZqhKHP <G00336837@gmit.ie>
2019-02-20 13:21:01 +0000

    Adding setter and getter methods for the confirmation message.

--- ProjectFrontEnd/src/app/Services/customerConfirmation.service.ts
+++ ProjectFrontEnd/src/app/Services/customerConfirmation.service.ts
@@ -6,5 +6,18 @@ import { Customer } from '../models/customer.model';
 })
 
 export class CustomerConfirmationService{
-    
+
+     constructor(){}
+
+     confirmationMessage: string;
+
+     setConfirmationMessage(message: string): void{
+        console.log("Setting confirmation message ---> " + message)
+        this.confirmationMessage = message;
+     }
+
+     getConfirmationMessage(): string{
+        return this.confirmationMessage
+     }
+
 }
