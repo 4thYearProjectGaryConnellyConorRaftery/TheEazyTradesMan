0497cf39262ccb99a4f98dcd4b131fe09b476d4d
ZqhKHP <G00336837@gmit.ie>
2019-02-23 17:27:38 +0000

    Accept jobs almost working.

--- ProjectFrontEnd/src/app/Worker/my-requests.component.ts
+++ ProjectFrontEnd/src/app/Worker/my-requests.component.ts
@@ -14,8 +14,11 @@ export class MyRequestsComponent implements OnInit {
 
   worker: Worker;
   jobs: Job[] = [];
+  acceptedJobs: Job[] = [];
   job: Job;
+  acceptedJob: Job;
   requests: string[];
+  accepts: string[];
 
   constructor(
    private workerService: WorkersService,
@@ -23,10 +26,17 @@ export class MyRequestsComponent implements OnInit {
    private router: Router) { }
 
   ngOnInit() {
-    this.workerService.getWorker(localStorage.getItem('WorkerID')).subscribe(data => {
+      this.workerService.getWorker(localStorage.getItem('WorkerID')).subscribe(data => {
       this.worker = data;
 
       this.requests = this.worker.jobsRequested.split(" ");
+      this.accepts = this.worker.jobsAccepted.split(" ");
+      console.log("This Worker ---> " + this.worker.id);
+
+     /* Need to loop through each string array to compare each element.
+      * If any of them match, add them to an array of stings.
+      *
+      */
 
      
 
@@ -34,19 +44,34 @@ export class MyRequestsComponent implements OnInit {
        console.log("Length of requests --->" + this.requests.length)
        console.log(this.requests[i].length)
        console.log("This is the request at " + i + " " + this.requests[i])
-      if(this.requests[i].length > 3){
+      if(this.requests[i].length > 1){ // 1 was just picked randomley because a check for null wasn't working.
           this.jobService.getJob(this.requests[i]).subscribe(data => {
             
             this.job = data;
-            console.log("////" + typeof this.job)
+            console.log("Number of jobs that were accepted for this worker ---> " + this.accepts.length)
             console.log("Request ID ---> " + this.job.id)
             console.log(this.job)
-            this.jobs.push(this.job)
+            if(this.job.accepted == false){
+              this.jobs.push(this.job) // So we only list requested jobs that have not yet been accepted.
+            }
+            
           
             //this.jobs[i] = this.job;
           })
       }
+    }// End for.
+    
+    for(var i = 0; i < this.accepts.length; i++){
+      console.log("Length of accepted jobs array ---> " + this.accepts.length)
+      console.log("---" + this.accepts[i] + "---")
+      if(this.accepts[i] != "null"){
+        this.jobService.getJob(this.accepts[i]).subscribe(data =>{
+          this.acceptedJob = data;
+          console.log("Accepted Job ID ---> " + this.acceptedJob.id)
+          this.acceptedJobs.push(this.acceptedJob)
+        });
       }
+    }
 
 
     })
