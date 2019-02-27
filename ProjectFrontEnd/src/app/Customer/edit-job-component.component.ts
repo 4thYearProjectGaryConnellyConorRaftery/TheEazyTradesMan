01f7f0e877b38aee37653f2ed06b1882f8ead6f9
ZqhKHP <G00336837@gmit.ie>
2019-02-27 21:27:12 +0000

    Starting work on edit jobs component.

--- ProjectFrontEnd/src/app/Customer/edit-job-component.component.ts
+++ ProjectFrontEnd/src/app/Customer/edit-job-component.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Job } from '../models/job.model';
+import { JobsService } from '../Services/jobs.service';
+import { Router, Params } from '@angular/router';
+import { AuthService } from '../core/auth.service';
+import { CustomerConfirmationService } from '../Services/customerConfirmation.service';
 
 @Component({
   selector: 'app-edit-job-component',
@@ -7,7 +13,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditJobComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private jobService: JobsService,
+    private confirmationService: CustomerConfirmationService, 
+    private router: Router, 
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
   }
