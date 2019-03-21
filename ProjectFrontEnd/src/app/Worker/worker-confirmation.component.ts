e54cb8524119bd1e74eb437100543f72d90066ec
conor <c-raftery@hotmail.com>
2019-03-21 11:35:07 +0000

    Worker confirmation finished

--- ProjectFrontEnd/src/app/Worker/worker-confirmation.component.ts
+++ ProjectFrontEnd/src/app/Worker/worker-confirmation.component.ts
@@ -3,6 +3,12 @@ import { WorkerConfirmationService } from '../Services/workerConfirmation.servic
 import { Router, Params } from '@angular/router';
 import { AuthService } from '../core/auth.service';
 
+/**
+ * WorkerConfirmationComponent is the components that gets rendered for
+ * the Worker whenever they make an action that requires some sort of
+ * confirmation message to be shown to them. This confirmation message
+ * can be set at runtime depending on the action that is being confirmed.
+ */
 @Component({
   selector: 'app-worker-confirmation',
   templateUrl: './worker-confirmation.component.html',
@@ -12,37 +18,67 @@ export class WorkerConfirmationComponent implements OnInit {
 
   message: string;
 
+  /**
+   * Null constructor
+   * @param authService 
+   * @param service 
+   * @param router 
+   */
   constructor(private authService: AuthService, private service: WorkerConfirmationService, private router: Router) { }
 
+  /**
+   * Get a handle on the current confirmation message.
+   */
   ngOnInit() {
-    /* 
-     * Get a handle on the current confirmation message.
-     */
     this.message = this.service.getConfirmationMessage();
   }
 
+  /**
+   * When the user clicks the "OK" button, redirect them to the listJobsWorker
+   * component.
+   */
   ok(): void{
     this.router.navigate(["/listJobsWorker"]);
   }
 
 
-   // Navigation.
+   /**
+    * When the user click on the "List Jobs" navigation button, redirect them
+    * to listJobsWorker component.
+    */
   navListJobs(): void{
     this.router.navigate(["/listJobsWorker"]);
   }
 
+  /**
+   * When the user clicks "View Profile" navigation button, redirect them to
+   * the viewProfile component.
+   */
    navMyProfile(): void{
     this.router.navigate(["/viewProfile"]);
   }
 
+  /**
+   * When the user clicks "Edit Profile" navigation button, redirect them to
+   * the editProfile component.
+   */
    navEditProfile(): void{
     this.router.navigate(["/editProfile"]);
   }
 
+  /**
+   * When the user clicks "My Jobs" navigation button, redirect them to
+   * the myrequests component.
+   */
   navMyRequests(): void{
     this.router.navigate(["/myrequests"]);
   }
 
+  /**
+   * When the user clicks the "Logout" navigation button, logout 
+   * that user and redirect them to the login page. Change the local
+   *  storage to remove access for site navigation.
+   */
    logout(){
     this.authService.doLogout()
     .then((res) => {
