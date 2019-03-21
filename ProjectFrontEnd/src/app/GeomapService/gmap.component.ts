0bd24236cff3b8b4289c96915bf807d5ad4f3534
conor <c-raftery@hotmail.com>
2019-03-21 12:19:01 +0000

    geomap completed

--- ProjectFrontEnd/src/app/GeomapService/gmap.component.ts
+++ ProjectFrontEnd/src/app/GeomapService/gmap.component.ts
@@ -5,54 +5,71 @@ import { GeocodeService } from './geocode.service';
 import { Location } from './location-model';
 import { Router, Params } from '@angular/router';
 
-
+/**
+ * This class is used for translating the the coordinates into a given point on the geomap.
+ */
 @Component({
   selector: 'my-gmap',
   templateUrl: './gmap.component.html',
-  styleUrls: [ './gmap.component.css' ] 
+  styleUrls: ['./gmap.component.css']
 })
 export class GmapComponent implements OnInit {
   address = 'London';
   location: Location;
   loading: boolean;
 
+  /**
+   * Null constructor
+   * @param geocodeService 
+   * @param ref 
+   * @param router 
+   */
   constructor(
     private geocodeService: GeocodeService,
     private ref: ChangeDetectorRef,
     private router: Router
-  ) {}
-  
+  ) { }
+
+  /**
+   * Display location when this class is ran.
+   */
   ngOnInit() {
     this.showLocation();
   }
 
-  back(){
+  /**
+   * Get handle on User ID when back navigation is selected.
+   */
+  back() {
     console.log("Customer ID --->" + localStorage.getItem('CustomerID'))
     console.log("Worker ID --->" + localStorage.getItem('WorkerID'))
 
-    if(localStorage.getItem('CustomerID') != "x"){
+    if (localStorage.getItem('CustomerID') != "x") {
       this.router.navigate(["/listJobs"])
     }
-    else if(localStorage.getItem('WorkerID') != "x"){
+    else if (localStorage.getItem('WorkerID') != "x") {
       this.router.navigate(["/listJobsWorker"])
     }
-    /*else{
-      this.router.na
-    }*/
   }
 
+  /**
+   * Display address location on Map.
+   */
   showLocation() {
     this.addressToCoordinates();
   }
 
+  /**
+   * Translate the given address to coordinates.
+   */
   addressToCoordinates() {
     this.loading = true;
     this.geocodeService.geocodeAddress(this.address)
-    .subscribe((location: Location) => {
+      .subscribe((location: Location) => {
         this.location = location;
         this.loading = false;
-        this.ref.detectChanges();  
-      }      
-    );     
+        this.ref.detectChanges();
+      }
+      );
   }
 }
--- ProjectFrontEnd/src/app/GeomapService/location-model.ts
+++ ProjectFrontEnd/src/app/GeomapService/location-model.ts
@@ -1,3 +1,6 @@
+/**
+ * Interface used for the longitute and latitude of the jobs address.
+ */
 export interface Location {
     lat: number; 
     lng: number;
