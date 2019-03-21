002eaf095faa975b1333a8d9a1901f024a000dbc
conor <c-raftery@hotmail.com>
2019-03-21 11:46:11 +0000

    List worker jobs finished.

--- ProjectFrontEnd/src/app/Worker/list-worker-jobs.component.ts
+++ ProjectFrontEnd/src/app/Worker/list-worker-jobs.component.ts
@@ -1,4 +1,3 @@
-// /// <reference types="@types/googlemaps" />
 import { Component, OnInit } from '@angular/core';
 import { Job } from '../models/job.model';
 import { Worker } from '../models/worker.model';
@@ -12,14 +11,17 @@ import { AuthService } from '../core/auth.service';
 import { CustomersService } from '../Services/customers.service';
 import { Customer } from '../models/customer.model';
 
-
+/**
+ * ListWorkerJobsComponent is the initial component that gets rendered when a Worker logs in.
+ * This component is only responsible for listing the Jobs for the Workers to see, this is
+ * to allow to worker to request a job they would like to complete.
+ */
 @Component({
   selector: 'app-list-worker-jobs',
   templateUrl: './list-worker-jobs.component.html',
   styleUrls: ['./list-worker-jobs.component.css']
 })
 export class ListWorkerJobsComponent implements OnInit {
-
 
   firstName: string = "";
   secondName: string = "";
@@ -30,6 +32,9 @@ export class ListWorkerJobsComponent implements OnInit {
   message: string = "";
   isWorker: boolean = false;
 
+  /**
+   * Initialize job object
+   */
   updateJob: Job ={
     id: "",
     trade: "",
@@ -46,6 +51,16 @@ export class ListWorkerJobsComponent implements OnInit {
 
   worker: Worker;
   
+  /**
+   * Null constructor
+   * @param workerService 
+   * @param jobService 
+   * @param confirmationService 
+   * @param router 
+   * @param geoMap 
+   * @param authService 
+   * @param customerService 
+   */
   constructor(
    private workerService: WorkersService,
    private jobService: JobsService, 
@@ -55,42 +70,33 @@ export class ListWorkerJobsComponent implements OnInit {
    private authService: AuthService,
    private customerService: CustomersService) { }
 
+   /**
+     * If the user is a Worker, then let them see the data.
+     */
   ngOnInit() {
-
     if(localStorage.getItem('WorkerID') == "x"){
       this.message = "Only logged in workers can view this page.(try refreshing)" // For testing.
       console.log("here" + this.message)
     }
     else{
-    this.isWorker = true;
-    
-    /*
-     * Get a handle on all of the jobs.
-     */
-    this.jobService.getJobs().subscribe(data => {
-      this.jobs = data
-      this.jobs.reverse()
-      /*
-      for(var i = 0; i < this.jobs.length; i++){
-        //this.id = this.jobs[i].customer
-        this.customerService.getCustomer(this.jobs[i].customer).subscribe(data =>{
-          console.log(i)
-          this.customer = data;
-          
-          this.jobs[this.x].customerName = this.customer.firstName + " " + this.customer.secondName ;
-          this.x++
-       
-        })
-      } */
-    })
+      this.isWorker = true;
+      
+      /**
+       * Get a handle on all of the jobs.
+       */
+      this.jobService.getJobs().subscribe(data => {
+        this.jobs = data
+        this.jobs.reverse()
+      })
+    }
   }
-  }
-  /*
+  /**
    * When the worker clicks request on a job.
    */
   request(job: Job): void{
     console.log("REQUEST FOR -->" + job.id);
-    /*
+
+    /**
      * Add this workers id to the requests string for this job.
      */
     job.requests += " " + localStorage.getItem('WorkerID');
@@ -107,7 +113,7 @@ export class ListWorkerJobsComponent implements OnInit {
     this.updateJob.contact = job.contact;
 
 
-    /*
+    /**
      * Update this job on the server side and navigate the user.
      */
     this.jobService.putJob(this.updateJob).subscribe((data: Job) =>{
@@ -115,7 +121,8 @@ export class ListWorkerJobsComponent implements OnInit {
       this.confirmationService.setConfirmationMessage("Job has been successfully requested!");
       this.router.navigate(["/workerConfirmation"]);
     });
-    /*
+
+    /**
      * Now add this jobs id to the jobsRequested string on the current worker
      * and update that worker on the server side.
      */
@@ -131,35 +138,51 @@ export class ListWorkerJobsComponent implements OnInit {
     
   }
 
- /*
-  * If the user wants to view the mao fir this job.
+ /**
+  * If the user wants to view the map for this job by clicking View Map.
   */
   getMap(job: Job){
-
     this.geoMap.setAddress(job.location);
     this.router.navigate(["/gmap"]);
-
   }
 
- 
-
-   // Navigation.
-  navListJobs(): void{
+  /**
+    * When the user click on the "List Jobs" navigation button, redirect them
+    * to listJobsWorker component.
+    */
+   navListJobs(): void{
     this.router.navigate(["/listJobsWorker"]);
   }
 
+  /**
+   * When the user clicks "View Profile" navigation button, redirect them to
+   * the viewProfile component.
+   */
    navMyProfile(): void{
     this.router.navigate(["/viewProfile"]);
   }
 
+  /**
+   * When the user clicks "Edit Profile" navigation button, redirect them to
+   * the editProfile component.
+   */
    navEditProfile(): void{
     this.router.navigate(["/editProfile"]);
   }
 
+  /**
+   * When the user clicks "My Jobs" navigation button, redirect them to
+   * the myrequests component.
+   */
   navMyRequests(): void{
     this.router.navigate(["/myrequests"]);
   }
 
+  /**
+   * When the user clicks the "Logout" navigation button, logout 
+   * that user and redirect them to the login page. Change the local
+   *  storage to remove access for site navigation.
+   */
    logout(){
     this.authService.doLogout()
     .then((res) => {
@@ -170,8 +193,6 @@ export class ListWorkerJobsComponent implements OnInit {
       console.log("Logout error", error);
     });
   }
-
-  // End Navigation.
 
 }
 
