13048b94d7facbab57ddd6971b3355f0aafec6bc
conor <c-raftery@hotmail.com>
2019-03-21 12:01:38 +0000

    my requests done

--- ProjectFrontEnd/src/app/Worker/my-requests.component.ts
+++ ProjectFrontEnd/src/app/Worker/my-requests.component.ts
@@ -9,13 +9,16 @@ import { AuthService } from '../core/auth.service';
 import { CustomersService } from '../Services/customers.service';
 import { Customer } from '../models/customer.model';
 
+/**
+ * My Requests component handles all the workers job requests. 
+ * When the worker requests a job and when a job request has been accepted.
+ */
 @Component({
   selector: 'app-my-requests',
   templateUrl: './my-requests.component.html',
   styleUrls: ['./my-requests.component.css']
 })
 export class MyRequestsComponent implements OnInit {
-
   worker: Worker;
   jobs: Job[] = [];
   acceptedJobs: Job[] = [];
@@ -27,182 +30,148 @@ export class MyRequestsComponent implements OnInit {
   requestSet = new Set();
   customer: Customer;
   myJobs: Job[] = [];
-  x: number = 0; 
+  x: number = 0;
   message: string = "";
   isWorker; boolean = false;
 
+  /**
+   * Null Constructor
+   * @param workerService 
+   * @param jobService 
+   * @param router 
+   * @param geoMap 
+   * @param authService 
+   * @param customerService 
+   */
   constructor(
-   private workerService: WorkersService,
-   private jobService: JobsService,
-   private router: Router,
-   private geoMap: GeocodeService,
-   private authService: AuthService,
-   private customerService: CustomersService) { }
+    private workerService: WorkersService,
+    private jobService: JobsService,
+    private router: Router,
+    private geoMap: GeocodeService,
+    private authService: AuthService,
+    private customerService: CustomersService) { }
 
+  /**
+    * If the user is a Worker, then let them see the data.
+    */
   ngOnInit() {
-
-    if(localStorage.getItem('WorkerID') == "x"){
+    if (localStorage.getItem('WorkerID') == "x") {
       this.message = "Only logged in workers can view this page." // For testing.
       console.log("here" + this.message)
     }
-    else{
-    this.isWorker = true;
-    
-    /*
-     * Get a handle on the current worker.
-     */
+    else {
+      this.isWorker = true;
+
+      /**
+       * Get a handle on the current worker.
+       */
       this.workerService.getWorker(localStorage.getItem('WorkerID')).subscribe(data => {
-      this.worker = data;
-      /*
-       * Split the workers requests and accepts into string arrays.
-       */
-      this.requests = this.worker.jobsRequested.split(" ");
-      this.accepts = this.worker.jobsAccepted.split(" ");
+        this.worker = data;
 
-      for(let request of this.requests){
-        this.requestSet.add(request)
-      }
+        /**
+         * Split the workers requests and accepts into string arrays.
+         */
+        this.requests = this.worker.jobsRequested.split(" ");
+        this.accepts = this.worker.jobsAccepted.split(" ");
 
-      for(let accept of this.accepts){
-        this.acceptSet.add(accept)
-      }
-     // console.log("This Worker ---> " + this.worker.id);
-
-      /*
-       * Populate the requests Job array by querying for each job from the database.
-       */
-      for(let request of this.requestSet){
-        
-      // console.log("Length of requests --->" + this.requests.length)
-     //  console.log(this.requests[i].length)
-      // console.log("This is the request at " + i + " " + this.requests[i])
-      if(request.length > 1){ // 1 was just picked randomley because a check for null wasn't working.
-          this.jobService.getJob(request).subscribe(data => {
-            
-            this.job = data;
-         
-            if(this.job.accepted == false){  // 
-        
-              console.log("JOB ---> " + this.job.id)
-         
-               this.jobs.push(this.job) 
-          
-            
-            }
-          })
-      }
-    }// End for.
-
-    for(let accept of this.acceptSet){
-      console.log("ACCEPT ---> " + accept)
-     // console.log(" Hey ---> " + accept)
-     if(accept != "null" || accept != ""){
-      this.jobService.getJob(accept).subscribe(data =>{
-        this.acceptedJob = data;
-        if(this.acceptedJob.complete == false){
-          
-            this.acceptedJobs.push(this.acceptedJob) 
-
-        
-          
+        for (let request of this.requests) {
+          this.requestSet.add(request)
         }
-         
-         // this.acceptedJobs.push(this.acceptedJob)
-          //console.log("INDEX --> " + this.acceptedJobs.indexOf(this.acceptedJob))
-        
-        
-      });
 
-     }
+        for (let accept of this.accepts) {
+          this.acceptSet.add(accept)
+        }
+
+        /**
+         * Populate the requests Job array by querying for each job from the database.
+         */
+        for (let request of this.requestSet) {
+
+          if (request.length > 1) { // 1 was just picked randomley because a check for null wasn't working.
+            this.jobService.getJob(request).subscribe(data => {
+              this.job = data;
+              if (this.job.accepted == false) {  // 
+                console.log("JOB ---> " + this.job.id)
+                this.jobs.push(this.job)
+              }
+            })
+          }
+        }// End for.
+
+        /**
+         * Populate the accepts Job array by querying for each job from the database.
+         */
+        for (let accept of this.acceptSet) {
+          console.log("ACCEPT ---> " + accept)
+          if (accept != "null" || accept != "") {
+            this.jobService.getJob(accept).subscribe(data => {
+              this.acceptedJob = data;
+              if (this.acceptedJob.complete == false) {
+                this.acceptedJobs.push(this.acceptedJob)
+              }
+            });
+          }
+        }
+
+      })
 
     }
-
-
-
-
-
-
-
-/*
-
-  /*
-     * Populate the accepts Job array by querying for each job from the database.
-     */
-  //  for(var i = 0; i < this.accepts.length; i++){
-      //  console.log("Length of accepted jobs array ---> " + this.accepts.length)
-   //     console.log("---" + this.accepts[i] + "---")
-       // console.log("INDEX --> " + this.accepts.includes(this.accepts[i]).valueOf())
-   //     if(this.accepts[i] != "null"){
-    //      this.jobService.getJob(this.accepts[i]).subscribe(data =>{
-      //      this.acceptedJob = data;
-          //  console.log("Accepted Job ID ---> " + this.acceptedJob.id)
-             
-       //       this.acceptedJobs.push(this.acceptedJob)
-              //console.log("INDEX --> " + this.acceptedJobs.indexOf(this.acceptedJob))
-            
-            
-      //    });
-     //   }
-   //   }
-  
-
-
-
-
-
-
-  
-
-
-
-
-
-
-
-
-
-    })
-
   }
-  }
-   /* 
-    * If the user wants to see the map.
-    */
-   getMap(job: Job){
 
+  /**
+  * If the user wants to view the map for this job by clicking View Map.
+  */
+  getMap(job: Job) {
     this.geoMap.setAddress(job.location);
     this.router.navigate(["/gmap"]);
-
   }
 
-
-  // Navigation.
-  navListJobs(): void{
+  /**
+     * When the user click on the "List Jobs" navigation button, redirect them
+     * to listJobsWorker component.
+     */
+  navListJobs(): void {
     this.router.navigate(["/listJobsWorker"]);
   }
 
-   navMyProfile(): void{
+  /**
+   * When the user clicks "View Profile" navigation button, redirect them to
+   * the viewProfile component.
+   */
+  navMyProfile(): void {
     this.router.navigate(["/viewProfile"]);
   }
 
-   navEditProfile(): void{
+  /**
+   * When the user clicks "Edit Profile" navigation button, redirect them to
+   * the editProfile component.
+   */
+  navEditProfile(): void {
     this.router.navigate(["/editProfile"]);
   }
 
-  navMyRequests(): void{
-    this.router.navigate(["myrequests"]);
+  /**
+   * When the user clicks "My Jobs" navigation button, redirect them to
+   * the myrequests component.
+   */
+  navMyRequests(): void {
+    this.router.navigate(["/myrequests"]);
   }
 
-   logout(){
+  /**
+   * When the user clicks the "Logout" navigation button, logout 
+   * that user and redirect them to the login page. Change the local
+   *  storage to remove access for site navigation.
+   */
+  logout() {
     this.authService.doLogout()
-    .then((res) => {
-       localStorage.setItem('WorkerID', "x")
-      this.router.navigate(["/login"]);
-    }, (error) => {
-     // console.log("Logout error", error);
-    });
+      .then((res) => {
+        //this.location.back(); //login
+        localStorage.setItem('WorkerID', "x")
+        this.router.navigate(["/login"]);
+      }, (error) => {
+        console.log("Logout error", error);
+      });
   }
-
-  // End Navigation.
 
 }
--- ProjectFrontEnd/src/app/Worker/view-profile.component.ts
+++ ProjectFrontEnd/src/app/Worker/view-profile.component.ts
@@ -4,13 +4,15 @@ import { WorkersService } from '../Services/workers.service';
 import { Router, Params } from '@angular/router';
 import { AuthService } from '../core/auth.service';
 
+/**
+ * View profile component is used to view the logged in workers profile when the page is opened.
+ */
 @Component({
   selector: 'app-view-profile',
   templateUrl: './view-profile.component.html',
   styleUrls: ['./view-profile.component.css']
 })
 export class ViewProfileComponent implements OnInit {
-
   worker: Worker;
   ratingArray: string[] = [];
   amount: number = null;
@@ -18,12 +20,18 @@ export class ViewProfileComponent implements OnInit {
   message: string = "";
   isWorker: boolean = false;
   
+  /**
+   * Null constructor
+   * @param workerService 
+   * @param router 
+   * @param authService 
+   */
   constructor(private workerService: WorkersService,  private router: Router, private authService: AuthService) { }
 
-  
-
+  /**
+     * If the user is a Worker, then let them see the data.
+     */
   ngOnInit() { 
-
     if(localStorage.getItem('WorkerID') == "x"){
       this.message = "Only logged in workers can view this page." // For testing.
       console.log("here" + this.message)
@@ -31,8 +39,9 @@ export class ViewProfileComponent implements OnInit {
     else{
     this.isWorker = true;
     
-    /* 
-     * Get a handle on the current worker to be displayed on the screen.
+    /** 
+     * Get a handle on the current workers profile and display their details on the screen.
+     * Also generates the workers rating via a calculation.
      */
     this.workerService.getWorker(localStorage.getItem('WorkerID')).subscribe(data =>{
       this.worker = data
@@ -55,25 +64,44 @@ export class ViewProfileComponent implements OnInit {
   }
   }
 
-  // Navigation.
-  navListJobs(): void{
+  /**
+    * When the user click on the "List Jobs" navigation button, redirect them
+    * to listJobsWorker component.
+    */
+   navListJobs(): void{
     this.router.navigate(["/listJobsWorker"]);
   }
 
+  /**
+   * When the user clicks "View Profile" navigation button, redirect them to
+   * the viewProfile component.
+   */
    navMyProfile(): void{
     this.router.navigate(["/viewProfile"]);
   }
 
+  /**
+   * When the user clicks "Edit Profile" navigation button, redirect them to
+   * the editProfile component.
+   */
    navEditProfile(): void{
     this.router.navigate(["/editProfile"]);
   }
 
+  /**
+   * When the user clicks "My Jobs" navigation button, redirect them to
+   * the myrequests component.
+   */
   navMyRequests(): void{
-    this.router.navigate(["myrequests"]);
+    this.router.navigate(["/myrequests"]);
   }
 
-  
-  logout(){
+  /**
+   * When the user clicks the "Logout" navigation button, logout 
+   * that user and redirect them to the login page. Change the local
+   *  storage to remove access for site navigation.
+   */
+   logout(){
     this.authService.doLogout()
     .then((res) => {
       //this.location.back(); //login
@@ -83,7 +111,4 @@ export class ViewProfileComponent implements OnInit {
       console.log("Logout error", error);
     });
   }
-
-  // End Navigation.
-
 }
