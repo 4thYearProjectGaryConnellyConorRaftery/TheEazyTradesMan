a341bcfa0d87edeba5d1e20dbaa91556d46af6c8
conor <c-raftery@hotmail.com>
2019-03-21 12:13:45 +0000

    geocode service

--- ProjectFrontEnd/src/app/GeomapService/geocode.service.ts
+++ ProjectFrontEnd/src/app/GeomapService/geocode.service.ts
@@ -1,5 +1,3 @@
-//https://stackblitz.com/edit/angular-google-maps-demo-geocoding?file=app%2Fapp.module.ts
-
 import { Injectable } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
 import { Observable } from 'rxjs';
@@ -11,60 +9,84 @@ import { Location } from './location-model';
 
 declare var google: any;
 
+/**
+ * This class is used to obtain a handle on the Geo Map service
+ *  provided by Google Maps. It is injected with the GeoMaps API.
+ */
 @Injectable()
 export class GeocodeService {
   private geocoder: any;
   address: string;
 
-  constructor(private mapLoader: MapsAPILoader) {}
+  /**
+   * Null constructor
+   * @param mapLoader 
+   */
+  constructor(private mapLoader: MapsAPILoader) { }
 
-setAddress(address: string): void{
-  this.address = address;
-}
+  /**
+   * Setter for setting Address of Job
+   * @param address 
+   */
+  setAddress(address: string): void {
+    this.address = address;
+  }
 
-getAddress(): string{
+  /**
+   * Getter for getting Address of Job
+   */
+  getAddress(): string {
+    return this.address;
+  }
 
-  return this.address;
-}
-
+  /**
+   * Initialize the Geocoder for GeoMap.
+   */
   private initGeocoder() {
     console.log('Init geocoder!');
     this.geocoder = new google.maps.Geocoder();
   }
 
+  /**
+   * Wait for map to load before adding a geolocation.
+   */
   private waitForMapsToLoad(): Observable<boolean> {
-    if(!this.geocoder) {
+    if (!this.geocoder) {
       return fromPromise(this.mapLoader.load())
-      .pipe(
-        tap(() => this.initGeocoder()),
-        map(() => true)
-      );
+        .pipe(
+          tap(() => this.initGeocoder()),
+          map(() => true)
+        );
     }
     return of(true);
   }
 
+  /**
+   * Obtain the address and translate it into geo code coordinates to be placed on the map.
+   * @param location 
+   */
   geocodeAddress(location: string): Observable<Location> {
     console.log('Start geocoding!');
     return this.waitForMapsToLoad().pipe(
       // filter(loaded => loaded),
       switchMap(() => {
         return new Observable(observer => {
-          this.geocoder.geocode({'address': this.address}, (results, status) => {
+          this.geocoder.geocode({ 'address': this.address }, (results, status) => {
             if (status == google.maps.GeocoderStatus.OK) {
               console.log('Geocoding complete!');
               observer.next({
-                lat: results[0].geometry.location.lat(), 
+                lat: results[0].geometry.location.lat(),
                 lng: results[0].geometry.location.lng()
               });
             } else {
-                console.log('Error - ', results, ' & Status - ', status);
-                observer.next({ lat: 0, lng: 0 });
+              console.log('Error - ', results, ' & Status - ', status);
+              observer.next({ lat: 0, lng: 0 });
             }
             observer.complete();
           });
-        })        
+        })
       })
     )
   }
-  
+
 }
